267b0959623ccc5ba8b9105fb18b324f45f6c74b
Feighen Oosterbroek <feighen@gmail.com>
2017-03-02 09:51:45 +0200

    Initial phing. Added function to write dweets

--- examples/dweetio.php
+++ examples/dweetio.php
@@ -1,12 +1,21 @@
 <?php
 namespace Noondaysun\Dweetio;
-//: Includes
-include_once dirname(realpath( __FILE__ )) . DIRECTORY_SEPARATOR . '..' . 
-DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Dweetio.php';
-//: End
+
+/**
+ * Example usage of Dweetio oject
+ */
+
+// : Includes
+include_once dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Dweetio.php';
+// : End
 $dweet = new \Noondaysun\Dweetio\Dweetio_Client();
 
-$thing = (string) 'temperature';
-$latest = $dweet->getLatestDweetFor($thing);
-$dweets = $dweet->getDweetsFor($thing);
-print_r($dweets);
+$thing = (string) $argv[1] ?? 'temperature';
+// $latest = $dweet->getLatestDweetFor($thing);
+// $dweets = $dweet->getDweetsFor($thing);
+
+$content = (array) [
+    'test' => 'number'
+];
+$success = $dweet->dweetFor($thing, $content, false);
+print_r($success);
